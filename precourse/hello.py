adf6c6a342c42ff6576028627fffb43fc300e8ce
scnkera <149810869+scnkera@users.noreply.github.com>
2024-08-24 23:38:45 -0400

    first commit

---
+++ precourse/hello.py
@@ -0,0 +1,5 @@
+# hello.py
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print(f"Hello, {name}. You are {age} years old!")
---
+++ precourse/variables_practice.py
@@ -0,0 +1,4 @@
+# The user's input will be stored in the variable called language
+language = input("What is your favorite programming language? ")
+
+print(f"{language} is my favorite too!")
